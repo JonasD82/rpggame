3b05b7ae18ae71521e9acd06bdd6ae86ca147eac
JonasD82 <112728274+JonasD82@users.noreply.github.com>
2023-04-05 18:07:17 -0700

    slow print

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-
+introtext = 'text intro to adventure game! \'you wake up in teh dark\', etc / I finally figured out how to do this in javascript and html... / / / oh boy :)'
 
 function button_off(id, on) {
     if (on == "off") {
@@ -8,18 +8,28 @@ function button_off(id, on) {
   }
 
 function print_slow() {
-    button_off("printbutton", "off")
+    // button_off("printbutton", "off")
     var $test = document.getElementById('gametext').innerHTML, $html = '', $i;
 
     for ($i = 0; $i < $test.length; $i++) {
-    $html += '<span style="animation: foo ' + $i + 's">' + ($test[$i]) + '</span>';
+        if ($test[$i] == "/") {
+            $html += '<br>'
+            continue}
+        else 
+           { $html += '<span style="animation: foo ' + $i + 's">' + ($test[$i]) + '</span>';}
     }
     document.getElementById('gamewindow').innerHTML = $html;
-    setTimeout(button_off, 2000, "continuebutton", "on") 
+    // setTimeout(button_off, 2000, "continuebutton", "on") 
 }
 
-function cont() {
-    document.getElementById('gamewindow').innerHTML = "";
-    button_off('continuebutton', 'off')
-    setTimeout(button_off, 2000, "printbutton", "on")
+// function cont() {
+//     document.getElementById('gamewindow').innerHTML = "";
+//     button_off('continuebutton', 'off')
+//     setTimeout(button_off, 2000, "printbutton", "on")
+// }
+
+function gamestart() {
+    document.getElementById('gametext').innerHTML = introtext
+    print_slow()
+
 }
